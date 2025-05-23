3a597fc39649ccfb3220becd22e3b26a5231d203
celiboy93 <97116174+celiboy93@users.noreply.github.com>
2025-05-23 18:28:34 +0630

    main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { exists } from "https://deno.land/std/fs/exists.ts"; // Import exists function
 
-const envUUID = Deno.env.get('UUID') || 'e5185305-1984-4084-81e0-f77271159c62';
+const envUUID = Deno.env.get('UUID') || '27848739-7e62-4138-9fd3-098a63964b6b';
 const proxyIP = Deno.env.get('PROXYIP') || '';
 const credit = Deno.env.get('CREDIT') || 'DenoBy-ModsBots';
 
